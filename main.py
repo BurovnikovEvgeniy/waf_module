4c4c1ba6e8b89e1a0ba90a21ebac4816448a3d32
Eugeny Burovnikov <eugeny.burovnikov@vk.team>
2024-06-25 09:55:29 +0300

    Add normalize_query

--- main.py
+++ main.py
@@ -43,6 +43,12 @@ def check_sql_injection(input_str, cheat_sheet):
     return False
 
 
+def normalize_query(query):
+    normalized_query = query.strip()
+    normalized_query = re.sub(r'\s+', ' ', normalized_query)
+    return normalized_query
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -52,12 +58,15 @@ def index():
 def search():
     query = request.form['query']
 
+    # Normalize the query
+    normalized_query = normalize_query(query)
+
     # Load SQL injection cheat sheet
     cheat_sheet_file = 'static/cheat_sheet.txt'
     cheat_sheet = load_cheat_sheet(cheat_sheet_file)
 
     # Check for SQL injection
-    if check_sql_injection(query, cheat_sheet):
+    if check_sql_injection(normalized_query, cheat_sheet):
         return "SQL Injection detected! Your IP address has been blocked."
     else:
         message = f'Searching for: {query}'
